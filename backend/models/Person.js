1d9c8d09337de4b29b0ff6f0935c4a939da8e195
maniksheikh <dev.maniksheikh@gmail.com>
2026-01-03 21:15:24 +0600

    create env and person

---
+++ backend/models/Person.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const personSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now }
+});
+
+const Person = mongoose.models.Person || mongoose.model('Person', personSchema);
+
+module.exports = Person;
