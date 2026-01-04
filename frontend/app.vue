0c99e3dc635826ec7a627fffdd0173cee4cf4790
maniksheikh <dev.maniksheikh@gmail.com>
2026-01-05 01:16:31 +0600

    main app update code

---
+++ frontend/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="min-h-screen bg-slate-950 text-white selection:bg-indigo-500/30">
+    <Navbar />
+    <div class="pt-16">
+      <NuxtRouteAnnouncer />
+      <NuxtPage />
+    </div>
+  </div>
+</template>
+
+
