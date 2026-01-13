8dd754c4d87223245b42d844bc09d5e4f84df1ae
Manik Sheikh <dev.maniksheikh@gmail.com>
2026-01-13 20:06:38 +0600

    login page update

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -84,10 +84,15 @@
             <button
               type="button"
               @click="handleGoogleSignIn"
-              class="w-full flex justify-center py-3 px-4 border border-transparent text-sm font-bold rounded-xl text-gray-900 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 transition-all transform hover:scale-[1.02] active:scale-[0.98] shadow-lg shadow-gray-500/20 disabled:opacity-50 cursor-pointer disabled:cursor-not-allowed"
+              class="w-full flex justify-center py-3 px-4 border border-white/20 text-sm font-bold rounded-xl text-white bg-white/5 hover:bg-white/10 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-white/50 transition-all transform hover:scale-[1.02] active:scale-[0.98] shadow-lg shadow-gray-900/20 disabled:opacity-50 cursor-pointer disabled:cursor-not-allowed"
             >
-              <svg class="w-5 h-5 mr-2" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path d="M12.24 10.285V14.4h6.806c-.275 1.767-2.087 5.97-6.806 5.97-4.119 0-7.437-3.34-7.437-7.468 0-4.127 3.318-7.466 7.437-7.466 2.091 0 3.839.715 5.206 2.072l3.056-3.057C19.451 3.697 16.865 2 12.24 2c-6.582 0-11.92 5.335-11.92 11.917 0 6.582 5.338 11.919 11.92 11.919 7.115 0 11.518-4.912 11.518-11.643 0-.78-.086-1.495-.199-2.198H12.24z"/>
+              <svg class="w-5 h-5 mr-2" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" xmlns:xlink="http://www.w3.org/1999/xlink" style="display: block;">
+                <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"></path>
+                <path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"></path>
+                <path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z
+                "></path>
+                <path fill="#34A853" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"></path>
+                <path fill="none" d="M0 0h48v48H0z"></path>
               </svg>
               Sign in with Google
             </button>
